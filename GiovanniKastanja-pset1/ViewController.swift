fad5a7d92dfd29d3ccf1be6a96afb1422abc3caa
Giovkast <giovkast@gmail.com>
2017-09-09 18:34:05 +0200

    First commit

---
+++ GiovanniKastanja-pset1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GiovanniKastanja-pset1
+//
+//  Created by Giovanni Kastanja on 09-09-17.
+//  Copyright © 2017 Giovanni Kastanja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
